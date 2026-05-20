5297eb0a043b176367cc4cb92d84aa501b1d5143
hafeez-star <hafeezullah4217@gmail.com>
2026-05-20 05:54:58 +0500

    Pdf-Folder

--- src/pages/PDFTools.jsx
+++ src/pages/PDFTools.jsx
@@ -1,10 +1,43 @@
-
+import Navbar from "../components/Navbar";
 import jsPDF from "jspdf";
 
-const downloadPDF = () => {
-  const doc = new jsPDF();
+const PDFTools = () => {
 
-  doc.text("Hello Hafeez 😎", 20, 20);
+  const downloadPDF = () => {
 
-  doc.save("file.pdf");
+    const doc = new jsPDF();
+
+    doc.text("Hello Hafeez 😎", 20, 20);
+
+    doc.save("toolhub.pdf");
+  };
+
+  return (
+    <div className="min-h-screen bg-gray-100">
+
+      <Navbar />
+
+      <div className="flex justify-center items-center p-10">
+
+        <div className="bg-white p-10 rounded-3xl shadow w-full max-w-md text-center">
+
+          <h1 className="text-4xl font-bold mb-6">
+            PDF Tools
+          </h1>
+
+          <button
+            onClick={downloadPDF}
+            className="bg-violet-600 text-white px-6 py-4 rounded-xl w-full"
+          >
+            Download PDF
+          </button>
+
+        </div>
+
+      </div>
+
+    </div>
+  );
 };
+
+export default PDFTools;
