cd5e66292dbe5e909dc65d230dc0aae13d732325
hafeez-star <hafeezullah4217@gmail.com>
2026-05-20 06:06:46 +0500

    ThemContx

---
+++ src/context/ThemeContext.jsx
@@ -0,0 +1,49 @@
+import {
+  createContext,
+  useEffect,
+  useState,
+} from "react";
+
+export const ThemeContext =
+  createContext();
+
+const ThemeProvider = ({ children }) => {
+
+  const [darkMode, setDarkMode] =
+    useState(false);
+
+  const toggleTheme = () => {
+
+    setDarkMode(!darkMode);
+  };
+
+  useEffect(() => {
+
+    if (darkMode) {
+
+      document.documentElement.classList.add(
+        "dark"
+      );
+
+    } else {
+
+      document.documentElement.classList.remove(
+        "dark"
+      );
+    }
+
+  }, [darkMode]);
+
+  return (
+    <ThemeContext.Provider
+      value={{
+        darkMode,
+        toggleTheme,
+      }}
+    >
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export default ThemeProvider;
